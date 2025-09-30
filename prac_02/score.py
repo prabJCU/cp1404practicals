885bc88d5687c37ee5e35439e0714afa020cf4ea
prabJCU <prabhsher.singh@my.jcu.edu.au>
2025-09-30 13:12:36 +1000

    Add program to determine score

--- prac_02/score.py
+++ prac_02/score.py
@@ -8,23 +8,22 @@ import random
 def main():
     # Ask user for score
     score = float(input("Enter score: "))
-    result = get_score_status(score)
-    print(result)
+    get_score_status(score)
 
     # Generate random score
     random_score = random.randint(0, 100)
-    print(f"Random score: {random_score} -> {get_score_status(random_score)}")
-
+    print(f"Random score: {random_score} -> ", end="")
+    get_score_status(random_score)
 
 def get_score_status(score):
     if score < 0 or score > 100:
-        return "Invalid score"
+        print("Invalid score")
     elif score >= 90:
-        return "Excellent"
+        print("Excellent")
     elif score >= 50:
-        return "Passable"
+        print("Passable")
     else:
-        return "Bad"
+        print("Bad")
 
 
 main()
