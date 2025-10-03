d5172d377c647470fd629f6dbdf09af9f6746b29
prabJCU <prabsingh6767@gmail.com>
2025-10-03 12:56:48 +1000

    Add examples using random module

---
+++ prac_03/random_examples.py
@@ -0,0 +1,14 @@
+"""
+CP1404 - Practical
+randoms
+"""
+
+# 17, the smallest number could have been 5, and the largest 20
+
+# 5, smallest number could have been 3, and the largest 9. Cannot produce number 4 as the range starts from 3 in steps of 2.
+
+# 4.259025306830869, smallest number could have been 2.5, and largest 5.5.
+
+import random
+
+print(random.randint(1, 100))
