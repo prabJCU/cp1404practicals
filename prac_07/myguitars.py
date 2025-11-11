0731b22ffe2e902f5cfde1adc951c2036c077017
prabJCU <prabsingh6767@gmail.com>
2025-11-11 13:40:57 +1000

    Add loading, sorting, and saving guitars to program

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -7,6 +7,16 @@ from prac_07.guitar import Guitar
 
 def main():
     guitars = []
+    load_guitars(guitars)
+
+    for guitar in guitars:
+        print(guitar)
+
+    add_guitar(guitars)
+    save_guitars(guitars)
+
+
+def load_guitars(guitars):
     in_file = open('guitars.csv', 'r')
     for line in in_file:
         parts = line.strip().split(',')
@@ -15,11 +25,25 @@ def main():
         cost = float(parts[2])
         guitar = Guitar(name, year, cost)
         guitars.append(guitar)
-        guitars.sort()
     in_file.close()
+    guitars.sort()
 
+
+def add_guitar(guitars):
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost} added.")
+        name = input("Name: ")
+
+
+def save_guitars(guitars):
+    out_file = open('guitars.csv', 'w')
     for guitar in guitars:
-        print(guitar)
+        print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+    out_file.close()
 
 
 main()
